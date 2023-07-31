f5220ef98673674afdcf73ce0eb270d7c234ba5c
cem-sucu <cems.cems.17@gmail.com>
2023-07-31 20:45:25 +0200

    debug submit ajout non finis

--- controller/ActionController.php
+++ controller/ActionController.php
@@ -6,13 +6,50 @@ use Model\Connect;
 
 class ActionController {
     public function ajouterFilm(){
+        if($_SERVER["REQUEST_METHOD"] === "POST"){
+            // Récupérer les données du formulaire
+            $titre = $_POST["titre"];
+            $anneSortie = $_POST["anneSortie"];
+            $duree = $_POST["duree"];
+            $realisateur_nom = $_POST["realisateur_nom"];
+            $realisateur_prenom = $_POST["realisateur_prenom"];
+            $affiche = $_POST["affiche"];
+    
+            // Insérer le nouveau film dans la base de données
+            $pdo = Connect::seConnecter();
+            if($pdo){
+                // Vérifier si le réalisateur existe déjà dans la base de données
+                $requete_select_realisateur = $pdo->prepare("SELECT id_realisateur FROM realisateur WHERE nom = ? AND prenom = ?");
+                $requete_select_realisateur->execute([$realisateur_nom, $realisateur_prenom]);
+                $realisateur = $requete_select_realisateur->fetch();
+    
+                // Si le réalisateur n'existe pas dans la base de données, l'ajouter
+                if(!$realisateur){
+                    $requete_insert_realisateur = $pdo->prepare("INSERT INTO personne (nom, prenom) VALUES (?, ?)");
+                    $requete_insert_realisateur->execute([$realisateur_nom, $realisateur_prenom]);
+                    $id_realisateur = $pdo->lastInsertId();
+                } else {
+                    $id_realisateur = $realisateur["id_realisateur"];
+                }
+    
+                // Insérer le nouveau film en utilisant l'identifiant du réalisateur
+                $requete_insert_film = $pdo->prepare("INSERT INTO film (titre, anneSortie, durée, id_realisateur, affiche) 
+                                         VALUES (?, ?, ?, ?, ?)");
+                $requete_insert_film->execute([$titre, $anneSortie, $duree, $id_realisateur, $affiche]);
+
+                // Rediriger vers la page de liste des films après l'ajout
+                header("Location: ?action=listFilms");
+                exit; // Ajoutez cette ligne pour terminer l'exécution du script après la redirection
+            }
+        }
+    
+        // Si la méthode HTTP est GET, alors on affiche le formulaire d'ajout
         ob_start();
         require "view/ajouterFilm.php";
         $contenu = ob_get_clean();
         return $contenu;
-        }
-    
     }
+}
 
 
 
--- view/ajouterFilm.php
+++ view/ajouterFilm.php
@@ -7,7 +7,27 @@
 </head>
 <body>
     
-TEST AJOUTER
+<form action="?action=ajouterFilm" method="post">
+        <label for="titre">Titre :</label>
+        <input type="text" name="titre" id="titre" required>
+
+        <label for="anneSortie">Année de sortie :</label>
+        <input type="number" name="anneSortie" id="anneSortie" required>
+
+        <label for="duree">Durée (en minutes) :</label>
+        <input type="number" name="duree" id="duree" required>
+
+        <label for="realisateur_nom">Nom du réalisateur :</label>
+        <input type="text" name="realisateur_nom" id="realisateur_nom" required>
+
+        <label for="realisateur_prenom">Prénom du réalisateur :</label>
+        <input type="text" name="realisateur_prenom" id="realisateur_prenom" required>
+
+        <label for="affiche">Affiche (URL de l'image) :</label>
+        <input type="text" name="affiche" id="affiche" required>
+
+        <input type="submit" value="Ajouter">
+    </form>
 
 </body>
 </html>
